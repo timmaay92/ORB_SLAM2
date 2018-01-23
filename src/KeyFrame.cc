3dc9b98f2cdf77931836c065f7ec396bdaba5e36
gdiazh <g.hernan.diaz@gmail.com>
2018-01-23 18:26:25 -0300

    saving

--- src/KeyFrame.cc
+++ src/KeyFrame.cc
@@ -40,7 +40,7 @@ KeyFrame::KeyFrame(Frame &F, Map *pMap, KeyFrameDatabase *pKFDB):
     mvInvLevelSigma2(F.mvInvLevelSigma2), mnMinX(F.mnMinX), mnMinY(F.mnMinY), mnMaxX(F.mnMaxX),
     mnMaxY(F.mnMaxY), mK(F.mK), mvpMapPoints(F.mvpMapPoints), mpKeyFrameDB(pKFDB),
     mpORBvocabulary(F.mpORBvocabulary), mbFirstConnection(true), mpParent(NULL), mbNotErase(false),
-    mbToBeErased(false), mbBad(false), mHalfBaseline(F.mb/2), mpMap(pMap)
+    mbToBeErased(false), mbBad(false), mHalfBaseline(F.mb/2), mpMap(pMap), mpNextKeyFrame(NULL),mpPreviousKeyFrame(NULL)
 {
     mnId=nNextId++;
 
@@ -834,16 +834,16 @@ void KeyFrame::load(Archive &ar, const unsigned int version)
 //    std::cout <<"mpNextKeyFrame " << mpNextKeyFrame << std::endl;
     if(mpPreviousKeyFrame) {
         ar & mpPreviousKeyFrame;
-        std::cout << "ok saving the mpPreviousKeyFrame" << std::endl;
-        std::cout <<"mpPreviousKeyFrame " << mpPreviousKeyFrame << std::endl;
+        //std::cout << "ok saving the mpPreviousKeyFrame" << std::endl;
+        //std::cout <<"mpPreviousKeyFrame " << mpPreviousKeyFrame << std::endl;
     }
     else{
         mpPreviousKeyFrame = NULL;
     }
     if(mpNextKeyFrame) {
         ar & mpNextKeyFrame;
-        std::cout << "ok saving the mpNextKeyFrame" << std::endl;
-        std::cout <<"mpNextKeyFrame " << mpNextKeyFrame << std::endl;
+        //std::cout << "ok saving the mpNextKeyFrame" << std::endl;
+        //std::cout <<"mpNextKeyFrame " << mpNextKeyFrame << std::endl;
     }
     else{
         mpNextKeyFrame = NULL;
